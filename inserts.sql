5fc5087318d3e16e23780bee9328e2d9c598d6c0
JuanDRK <106005297+JuanDRK@users.noreply.github.com>
2022-05-31 12:17:03 -0500

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+--Inserción de datos en la tabla1--
+INSERT into tabla1 VALUES ('a', 'b'), ('aa', 'bb');
+
+--Inserción de datos en la tabla3--
+INSERT into tabla3 VALUES ('x', 1, 'w'), ('x', 2, 'z');
+
+--Inserción de datos en la tabla2--
+INSERT into tabla2 values ('a', 'x', 1), ('a', 'x', 2);
